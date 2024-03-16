2ea00aa15add02871740083119e4220457b63fce
tibineeva <tibineeva44@gmail.com>
2024-03-16 18:36:40 +0500

    add main page and nav

---
+++ src/routes/blog/posts/2/+page.svelte
@@ -0,0 +1,5 @@
+<h1>My Second Blog Post</h1>
+
+<p>Hello, lovely people! Today, I had a moment of inspiration and decided to remind everyone about the importance of self-care. In the hustle and bustle of daily life, it's crucial to take a step back, breathe, and prioritize our well-being. Whether it's indulging in a good book, going for a walk in nature, or simply relaxing with your favorite music, remember to carve out time for self-care. You deserve it, and your mind and body will thank you for it!</p>
+
+<p>Furthermore, I've been reflecting on gratitude lately. Taking a moment each day to appreciate the little things can truly shift your perspective and bring more positivity into your life. Gratitude has a way of magnifying joy and contentment. So, let's spread gratitude like confetti and watch how it transforms our days. Remember, even on tough days, there's always something to be thankful for. Let's cultivate an attitude of gratitude and watch the magic unfold! 🌟</p> 
--- src/routes/blog/routes.js
+++ src/routes/blog/routes.js
@@ -1,7 +1,12 @@
 export const routes = [
     {
-    id: 1,
-    title: 'My First Blog Post',
-    body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem ea ex maiores minima minus nisi perspiciatis sapiente voluptate',
+        id: 1,
+        title: 'My First Blog Post',
+        body: 'The first post discusses embarking on a new creative project and embracing opportunities for personal growth and creativity. It encourages chasing dreams and exploring new horizons.',
+    },
+    {
+        id: 2,
+        title: "My Second Blog Post",
+        body: "The second post focuses on the importance of self-care and gratitude in daily life. It emphasizes taking time for oneself and practicing gratitude to enhance well-being and positivity.",
     }
-    ];
+];
